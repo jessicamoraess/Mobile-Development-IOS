74ccda9331e6bf495c67fcdd113d95977d19389f
Jessica Moraes <jessicamoraes2@hotmail.com>
2018-04-08 00:36:40 -0300

    Commit Files - Projetos IOS

---
+++ 06-exercicio-IMC.swift
@@ -0,0 +1,147 @@
+﻿//: Playground - noun: a place where people can play 
+
+  
+
+import UIKit 
+
+  
+
+// 1 - Criar um Enum Com Tipos de IMC 
+
+enum IMC : String { 
+
+    case Low,Medium,High 
+
+} 
+
+  
+
+//2 - Criar um Struct Pessoa 
+
+struct Person{ 
+
+    var name: String 
+
+    var age : Int 
+
+    var gender: String 
+
+    var heigth : Double 
+
+    var weigth : Double 
+
+    var imc : String? 
+
+     
+
+    mutating func changeImc(newImc : String){ 
+
+        imc = newImc 
+
+    } 
+
+} 
+
+  
+
+// 3 - Criar um Metodo Fora da Struct 
+
+func calcImc(heigth:Double,weigth:Double) ->String{ 
+
+    let result =  weigth/pow(heigth, 2) 
+
+    switch result { 
+
+    case 0 ... 20: 
+
+        return IMC.Low.rawValue; 
+
+    default: 
+
+        return IMC.Medium.rawValue; 
+
+    } 
+
+} 
+
+  
+
+// Iniciar o Struct com Valores pre - definidos 
+
+let person1 = Person(name: "Jessica", age: 21, gender: "F", heigth: 1.73, weigth: 65.0, imc: nil) 
+
+  
+
+print(person1) 
+
+  
+
+// Passar Novos Valores por Copia 
+
+// 4- Interligar Tudo (Struct + Func Externa para Calc do IMC ) 
+
+var personValue = person1 
+
+personValue.name = "Teste" 
+
+personValue.age= 22 
+
+personValue.gender = "M" 
+
+personValue.heigth= 1.73 
+
+personValue.weigth= 65.0 
+
+personValue.changeImc(newImc: calcImc(heigth: personValue.heigth, weigth: personValue.weigth)) 
+
+  
+
+print(personValue) 
+
+  
+
+  
+
+// 5 - Criando um  Array de Pessoa(Struct) 
+
+var peopleList : [Person] = [] 
+
+  
+
+//Populando pessoaList Preenchendo o IMC por Funcao externa 
+
+var cont = 1 
+
+  
+
+while cont <= 10 { 
+
+    personValue.name = "teste" + cont.description 
+
+    personValue.age = 21 
+
+    personValue.gender = "M" 
+
+    personValue.heigth = Double(arc4random_uniform(100)) 
+
+    personValue.weigth = Double(arc4random_uniform(100)) 
+
+    personValue.changeImc(newImc: calcImc(altura: teste.altura, peso: teste.peso)) 
+
+    peopleList.append(personValue) 
+
+    cont += 1 
+
+} 
+
+  
+
+// Mostrar resultado com imc prenchido 
+
+for people  in peopleList{ 
+
+    print("nome \(pople.name) tem um imc \(people.imc)") 
+
+} 
+
+  
---
+++ 06-exercicio-IMC2.swift
@@ -0,0 +1,69 @@
+﻿import Foundation 
+
+//import UIKit 
+
+  
+
+struct IMC{ 
+
+  var weigth: Double 
+
+  var heigth: Double 
+
+  
+
+} 
+
+  
+
+let person = IMC(weigth: 50, heigth: 1.70) 
+
+  
+
+func calculateIMC(weigth: Double, heigth: Double) -> Double{ 
+
+  return weigth / (heigth * heigth)  
+
+   
+
+  
+
+} 
+
+  
+
+let imcPerson = calculateIMC(weigth: person.weigth, heigth: person.heigth) 
+
+  
+
+switch imcPerson{ 
+
+  case 0..<18.5: 
+
+  print("abaixo do peso") 
+
+  case 18..<25.3: 
+
+  print("peso ideal") 
+
+  case 25..<29.1: 
+
+  print("acima do peso") 
+
+  case 29..<39.2: 
+
+  print("obeso") 
+
+  default: 
+
+  print("nenhuma das opções") 
+
+   
+
+  print (imcPerson) 
+
+   
+
+   
+
+  } 
